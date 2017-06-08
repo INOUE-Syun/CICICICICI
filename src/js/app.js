af7664d76d3ca9389107b6e3409b8abb72ff4b29
INOUE-Syun <xinoue.syun@contract.isid.co.jp>
2017-06-08 16:48:57 +0900

    fibonacci

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,18 @@
 import helloAnything from './hello';
+import * as util from './utils';
 
 let app = document.getElementById('app');
-let p = document.createElement('p');
 
-p.textContent = helloAnything('Circle-CI');
+// hello
+let hello = document.createElement('p');
+hello.textContent = helloAnything('Circle-CI');
+app.appendChild(hello);
 
-app.appendChild(p);
+// utils
+console.time('fibonacci no memory');
+console.log(`fibonacci: ${util.fibonacci(30)}`);
+console.timeEnd('fibonacci no memory');
+
+console.time('fibonacci on memory');
+console.log(`fibonacci_onmemory: ${util.fibonacci_on_memory(30)}`);
+console.timeEnd('fibonacci on memory');
